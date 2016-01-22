1128bca4a22453effdbbfab7dfa1f5a907b753e9
Kysamaa <Alex.zwiebel@web.de>
2016-01-22 18:26:17 +0100

    test

--- Aka´s Draven/Program.cs
+++ Aka´s Draven/Program.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Enumerations;
 using EloBuddy.SDK.Events;
-using EloBuddy.SDK.Menu;
-using EloBuddy.SDK.Menu.Values;
-using EloBuddy.SDK.Rendering;
-using SharpDX;
 
 namespace AkaDraven
 {
